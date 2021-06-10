515e66445338e0eb28fcb837791b4b0019a1b5b1
William Leung <w.leung.93@gmail.com>
2021-06-09 22:15:11 -0700

    comments

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 - How long you spent on the project
-    - 1.5 ~ 2 hour (mostly spent watching the game play out)
+    - 1 ~ 2 hour (mostly spent watching the game play out)
 - Your Dino's high score: 
     - HI: 09946
+
 - Explanation of any struggles of the assignment (if any)
-    - I feel I may be cheating by setting the speed for the startJump() method to 300.
+    - Understanding how HTML Canvas animation works
+        - https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_animations
+    - Setting the speed for the startJump() method to 300 may be cheating...
+    - If you initialize the game the gameOver function deleted, you can run forever
--- dino.js
+++ dino.js
@@ -13,23 +13,23 @@ const game = Runner.instance_;
 
   //The obstacle's distance i determined the dino jumps
   let jumpDistance = 110;
-  
 
   //ensures a obstacles on canvas and the closest obj is within jump range.
   if( obstacles.length > 0 && obstacles[0].xPos <= jumpDistance){
 
       //the next obstacle coming towards tRex
       let obsType = obstacles[0].typeConfig.type;
+
       //the next obstacle height position
       let obsHeight = obstacles[0].yPos;
 
+      //jumps over any incomming cactus
       if(obsType === "CACTUS_SMALL" || obsType === "CACTUS_LARGE"){
           tRex.startJump(300)
 
+      //jumps over any incomming PTERODACTYL under a certain elevation
       }else if(obsType === "PTERODACTYL" && obsHeight > 50){
           tRex.startJump(300)
-      }else if (obsType === "PTERODACTYL" && obsHeight <= 50){
-        shouldDuck = true;
       }
   }
 
